10bd4af3f8d0459881b6b4b0f2c1d040ab29bd30
samuelmideksa <113627346+samuelmideksa@users.noreply.github.com>
2024-03-22 11:58:17 +0300

    custom user model users.User created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Custom User Model"""
+    
+    MALE = 'male'
+    FEMALE = 'female'
+
+    GENDER_CHOICES = (MALE, 'Male', FEMALE, 'Female')
+
+    USD = 'usd'
+    ETB = 'etb'
+
+    CURRENCY_CHOICES = (USD, 'usd', ETB, 'etb')
+
+    avatar = models.ImageField(upload_to='', blank=True, null=True)
+    birth_date = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True, null=True)
+    bio = models.TextField(null=True, blank=True)
+    currency = models.CharField(max_length=10, choices=CURRENCY_CHOICES, blank=True, default=ETB)
+    is_host = models.BooleanField(default=False)
+
+
