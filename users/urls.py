aa7666c9a5233398d52df2e25c955c85e5624f49
samuelmideksa <113627346+samuelmideksa@users.noreply.github.com>
2024-03-23 08:08:36 +0300

    signup and login setup

---
+++ users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from users import views as users_views
+
+urlpatterns = [
+    path('accounts', include('django.contrib.auth.urls')),
+    path('accounts/signup', users_views.SignUpView, name='signup'),
+]
