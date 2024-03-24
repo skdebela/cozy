0006b3f2648a2782479f3454d8de59203c809c84
samuelmideksa <113627346+samuelmideksa@users.noreply.github.com>
2024-03-24 10:47:45 +0300

    base template added

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from users import views as users_views
 
 urlpatterns = [
-    path('accounts', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup', users_views.SignUpView.as_view(), name='signup'),
 ]
