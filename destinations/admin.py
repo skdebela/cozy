313f67973cac9a7499a0bc3fe0d0d77bff0c1270
samuelmideksa <113627346+samuelmideksa@users.noreply.github.com>
2024-03-24 23:42:04 +0300

    multiselectfield installed + property models changed to multiselect attributes

--- destinations/admin.py
+++ destinations/admin.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
-from destinations.models import State, City, Destination, DestinationType, Amenity, Facility, HouseRule, Photo
+from destinations.models import Destination, Photo
 
-
-admin.site.register(State)
-admin.site.register(City)
 admin.site.register(Destination)
-admin.site.register(DestinationType)
-admin.site.register(Amenity)
-admin.site.register(Facility)
-admin.site.register(HouseRule)
+
 admin.site.register(Photo)
 
