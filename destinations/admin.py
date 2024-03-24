b32cddd6efb359fd0d62d92aad97dc04d9027594
samuelmideksa <113627346+samuelmideksa@users.noreply.github.com>
2024-03-24 16:37:54 +0300

    destination models created

--- destinations/admin.py
+++ destinations/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from destinations.models import State, City, Destination, DestinationType, Amenity, Facility, HouseRule, Photo
 
-# Register your models here.
+
+admin.site.register(State)
+admin.site.register(City)
+admin.site.register(Destination)
+admin.site.register(DestinationType)
+admin.site.register(Amenity)
+admin.site.register(Facility)
+admin.site.register(HouseRule)
+admin.site.register(Photo)
+
--- destinations/models.py
+++ destinations/models.py
@@ -1,3 +1,90 @@
 from django.db import models
+from core.models import AbstractTimeStamp
+from users.models import User
 
-# Create your models here.
+
+class AbstractItem(AbstractTimeStamp):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class DestinationType(AbstractItem):
+    pass
+
+
+class Amenity(AbstractItem):
+    pass
+
+
+class Facility(AbstractItem):
+    pass
+
+
+class HouseRule(AbstractItem):
+    pass
+
+
+class State(AbstractItem):
+    pass
+
+
+class City(AbstractItem):
+    pass
+
+
+class Destination(AbstractItem):
+    description = models.TextField()
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    address = models.CharField(max_length=140)
+    guests_capacity = models.IntegerField()
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(
+        User, related_name="destinations", on_delete=models.CASCADE
+    )
+    destination_type = models.ForeignKey(
+        DestinationType, related_name="destinations", on_delete=models.SET_NULL, null=True
+    )
+    amenities = models.ManyToManyField(Amenity, related_name="destinations", blank=True)
+    facilities = models.ManyToManyField(Facility, related_name="destinations", blank=True)
+    house_rules = models.ManyToManyField(HouseRule, related_name="destinations", blank=True)
+
+    # fees in etb
+    daily_price = models.DecimalField(max_digits=10, decimal_places=2)
+    weekly_discount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    monthly_discount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    cleaning_fee = models.DecimalField(max_digits=10, decimal_places=2 , null=True, blank=True)
+
+    def get_total_cost(self, duration=1):
+        """
+        calculate the total price based on duration of stay.
+        """
+
+        total_price = 0
+
+        if duration in range(7):
+            total_price += self.daily_price * duration
+        elif duration in range(7, 30):
+            total_price += ((self.daily_price * duration) - (self.weekly_discount * (duration / 7)))
+        elif duration >= 30:
+            total_price += ((self.daily_price * duration) - (self.monthly_discount * (duration / 30)))
+
+        total_price += self.cleaning_fee
+
+        return total_price
+
+
+class Photo(AbstractTimeStamp):
+    caption = models.CharField(max_length=100)
+    file = models.ImageField(upload_to='')
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE, related_name='photos')
+
