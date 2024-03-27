7909d071cadaf27e1a4d5a5d8c9f9e44baffd1f4
samuelmideksa <113627346+samuelmideksa@users.noreply.github.com>
2024-03-27 11:21:40 +0300

    hosting pages added

--- users/views.py
+++ users/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from users.models import SavedDestination
+from users.models import SavedDestination, User
+from destinations.models import Destination
 
 
 class SignUpView(CreateView):
@@ -18,3 +19,21 @@ def saved(request):
     saved_destinations = SavedDestination.objects.filter(user=user)
 
     return render(request, 'users/saved.html', {'saved_destinations': saved_destinations})
+
+
+def profile(request, username):
+    context = {}
+    user = User.objects.get(username=username)
+    context['user'] = user
+
+    if user.is_host:
+        user_listings = Destination.objects.filter(host=user).order_by('name')
+        context['user_listings'] = user_listings
+
+    return render(request, 'users/profile.html', context)
+
+
+def listings(request):
+    listing_destinations = Destination.objects.filter(host=request.user).order_by('name')
+
+    return render(request, 'users/listings.html', {'listing_destinations': listing_destinations})
