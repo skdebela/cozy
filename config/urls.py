14640854d3cab0f2156966ada932d7c389d9bcae
samuelmideksa <113627346+samuelmideksa@users.noreply.github.com>
2024-03-29 20:47:35 +0300

    booking app urls added

--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('', include('destinations.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('bookings.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
