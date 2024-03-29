3a3c0020282beea9663376d8425a4880466c8358
samuelmideksa <113627346+samuelmideksa@users.noreply.github.com>
2024-03-29 20:49:01 +0300

    DestinationForm updated to include PhotoFromSet

--- destinations/forms.py
+++ destinations/forms.py
@@ -2,37 +2,26 @@ from django import forms
 from destinations.models import Destination, Photo
 
 
-class DestinationSearchForm(forms.Form):
-    city = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'City'}))
-    check_in = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}))
-    check_out = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}))
-    guests = forms.ChoiceField(required=False, choices=[(i, i) for i in range(1, 16)] + [('15+', '15+')])
-
-
 class DestinationForm(forms.ModelForm):
-    # Field for destination photos
-    photos = forms.FileField(label='Upload Destination Photos',
-                             widget=forms.TextInput(attrs={
-                                 "name": "images",
-                                 "type": "File",
-                                 "class": "form-control",
-                                 "multiple": "True",
-                             }),
-                             required=False)
-
     class Meta:
         model = Destination
         fields = "__all__"
-        exclude = ['created_at', 'updated_at', 'host']
+        exclude = ['host']
 
-    def save(self, commit=True):
-        # Save the destination instance
-        destination = super().save(commit=False)
-        if commit:
-            destination.save()
 
-        # Handle photos upload
-        if self.cleaned_data.get('photos'):
-            for photo in self.cleaned_data['photos']:
-                Photo.objects.create(destination=destination, file=photo, caption=photo.name)
-        return destination
+class PhotoForm(forms.ModelForm):
+    class Meta:
+        model = Photo
+        fields = ['file', 'caption']
+
+
+PhotoFormSet = forms.inlineformset_factory(
+    Destination, Photo, form=PhotoForm, extra=5, can_delete=False
+)
+
+
+class DestinationSearchForm(forms.Form):
+    city = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'City'}))
+    # check_in = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}))
+    # check_out = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}))
+    guests = forms.ChoiceField(required=False, choices=[(i, i) for i in range(1, 16)] + [('15+', '15+')])
--- destinations/views.py
+++ destinations/views.py
@@ -1,27 +1,69 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 from django.shortcuts import render, redirect
+
+from bookings.forms import BookingForm
 from destinations.models import Destination
-from destinations.forms import DestinationSearchForm, DestinationForm
-from users.models import User
+from destinations.forms import DestinationSearchForm, DestinationForm, PhotoFormSet
+from users.models import User, SavedDestination
 
 
 def home_view(request):
     destinations = Destination.objects.all()
     search_form = DestinationSearchForm()
 
+    if request.user.is_authenticated:
+        for destination in destinations:
+            destination.is_saved_by_user = destination.saveddestination_set.filter(user=request.user).exists()
+
     context = {
         'destinations': destinations,
         'search_form': search_form
     }
+
+    if request.user.is_authenticated and request.method == 'POST' and 'save' in request.POST:
+        destination = Destination.objects.get(id=request.POST['destination_id'])
+        current_user = request.user
+        SavedDestination.objects.create(user=current_user, destination=destination)
+        return redirect('home')
+
+    if request.user.is_authenticated and request.method == 'POST' and 'delete' in request.POST:
+        current_user = request.user
+        destination = Destination.objects.get(id=request.POST['destination_id'])
+        saved_destination = SavedDestination.objects.get(user=current_user, destination=destination)
+        saved_destination.delete()
+        return redirect('home')
 
     return render(request, 'destinations/list.html', context)
 
 
 def details(request, destination_id, destination_name):
     destination = Destination.objects.get(id=destination_id)
+    search_form = DestinationSearchForm()
+    booking_form = BookingForm()
 
-    return render(request, 'destinations/details.html', {'destination': destination})
+    context = {
+        'destination': destination,
+        'search_form': search_form,
+        'booking_form': booking_form
+    }
+
+    if request.method == 'POST':
+        booking_form = BookingForm(request.POST)
+        if booking_form.is_valid():
+            booking = booking_form.save(commit=False)
+            booking.guest = request.user
+            booking.destination = destination
+            booking.save()
+
+            # Redirect to the booking page with the booking_id
+            return redirect('book', destination_id=destination_id, destination_name=destination_name,
+                            booking_id=booking.id)
+
+    # Reassign the booking_form after processing the POST request
+    context['booking_form'] = booking_form
+
+    return render(request, 'destinations/details.html', context)
 
 
 def search(request):
@@ -43,17 +85,20 @@ def search(request):
 
 @login_required
 def create(request):
+    current_user = User.objects.get(username=request.user.username)
     if request.method == 'POST':
-        form = DestinationForm(request.POST, request.FILES)
-        user = User.objects.get(username=request.user.username)
-        if form.is_valid():
-            destination = form.save(commit=False)
+        destination_form = DestinationForm(request.POST, request.FILES)
+        photo_formset = PhotoFormSet(request.POST, request.FILES)
+        if destination_form.is_valid() and photo_formset.is_valid():
+            destination = destination_form.save(commit=False)
             destination.host = request.user
             destination.save()
+            photo_formset.instance = destination
+            photo_formset.save()
 
-            if not user.is_host:
-                user.is_host = True
-                user.save()
+            if not current_user.is_host:
+                current_user.is_host = True
+                current_user.save()
 
             if 'save' in request.POST:
                 return redirect('listings')
@@ -61,30 +106,46 @@ def create(request):
                 return redirect('create_destination')
 
     else:
-        form = DestinationForm()
-        return render(request, 'destinations/create.html', {'form': form})
+        destination_form = DestinationForm()
+        photo_formset = PhotoFormSet()
+        return render(request, 'destinations/create.html',
+                      {'destination_form': destination_form, 'photo_formset': photo_formset})
 
 
 @login_required
 def update(request, destination_id, destination_name):
     destination = Destination.objects.get(id=destination_id)
-    form = DestinationForm(instance=destination)
 
-    if not destination.host.username == request.user.username:
+    # Check if the current user is the host of the destination
+    if not destination.host == request.user:
         return HttpResponseForbidden("You do not have permission to access this page.")
 
+    # Get the existing photos for the destination
+    existing_photos = destination.photos.all()
+
     if request.method == 'POST':
-        form = DestinationForm(request.POST, request.FILES, instance=destination)
-        if form.is_valid():
-            form.save()
+        destination_form = DestinationForm(request.POST, request.FILES, instance=destination)
+        photo_formset = PhotoFormSet(request.POST, request.FILES, instance=destination)
 
-        return redirect('listings')
+        if destination_form.is_valid() and photo_formset.is_valid():
+            destination_form.save()
+            photo_formset.save()
 
-    return render(request, 'destinations/update.html', {'form': form, 'destination': destination})
+            return redirect('listings')
+    else:
+        destination_form = DestinationForm(instance=destination)
+        photo_formset = PhotoFormSet(instance=destination)
+
+    return render(request, 'destinations/update.html', {
+        'destination_form': destination_form,
+        'photo_formset': photo_formset,
+        'destination': destination,
+        'existing_photos': existing_photos,  # Pass the existing photos to the template
+    })
 
 
 @login_required
-def delete(request, destination_id):
+def delete(request, destination_id, destination_name):
     destination = Destination.objects.get(id=destination_id)
 
     if not destination.host == request.user:
