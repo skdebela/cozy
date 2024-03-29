8925336307ee97066c1d334970d44125d3cd7364
samuelmideksa <113627346+samuelmideksa@users.noreply.github.com>
2024-03-29 20:41:14 +0300

    BookingForm created

---
+++ bookings/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from bookings.models import Booking
+
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['check_in', 'check_out']
+        widgets = {
+            'check_in': forms.DateInput(attrs={'type': 'date'}),
+            'check_out': forms.DateInput(attrs={'type': 'date'}),
+        }
